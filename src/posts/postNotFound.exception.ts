bd507c8fa3859a03a32e9e9f57f4e965d4825ed6
anthonymunene <munene.anthony@gmail.com>
2019-01-25 12:14:15 +0000

    new expections

---
+++ src/posts/postNotFound.exception.ts
@@ -0,0 +1,9 @@
+import HttpException from "exceptions/HttpException";
+
+class PostnotFoundException extends HttpException {
+ constructor(id: string) {
+     super(404, `Post with id ${id} not found`)
+ }   
+}
+
+export default PostnotFoundException
